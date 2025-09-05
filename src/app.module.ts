88cffd69eabf81971fc6d74406c8d9c172cba500
Chika 2021 <amaechichika9@gmail.com>
2025-09-05 21:27:00 +0100

    Fix Connection issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { Project } from './project/entities/project.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || '66.241.125.53' || 'localhost',
       port: Number(process.env.DB_PORT) || 3306,
       username: process.env.DB_USER || 'root',
       password: process.env.DB_PASS || '',
