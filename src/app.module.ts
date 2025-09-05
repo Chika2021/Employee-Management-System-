f64c3c1c43f023610f3362e8018811ac76b84d22
Chika 2021 <amaechichika9@gmail.com>
2025-09-06 00:09:44 +0100

    Fix Connection issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,17 @@ import { Project } from './project/entities/project.entity';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: process.env.DB_HOST || 'mysql.lytehosting.com'|| 'localhost',
-    //   port: Number(process.env.DB_PORT) || 3306,
-    //   username: process.env.DB_USER || 'root',
-    //   password: process.env.DB_PASS || '',
-    //   database: process.env.DB_NAME || 'employee_db',
-    //   entities: [User, Client, Project],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      // host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || '',
+      database: process.env.DB_NAME || 'employee_db',
+      entities: [User, Client, Project],
+      // synchronize: true,
+    }),
     UsersModule,
     ClientModule,
     ProjectModule,
